94a5434570eb3d1bac4fd16cbd2b6a29932a9362
Aashiq Abrahams <aashiqabrahamszb@gmail.com>
2019-03-06 22:41:20 +0200

    added more tests

--- src/main/java/com/aashiq/Person.java
+++ src/main/java/com/aashiq/Person.java
@@ -4,12 +4,22 @@ package com.aashiq;
 public class Person
 {
     private String name;
+    private boolean male = false;
 
     public Person(String name){
         this.name = name;
     }
 
     public Object getName() {
+
         return name;
     }
+
+    public void printGenderMessage(){
+        if(!male){
+            throw new IllegalStateException();
+        }
+        System.out.println("I am male!");
+    }
+
 }
